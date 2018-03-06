2cbc3cda712d9a5b3d2b45f2574a80a111bb8979
DuPeng <wy707086125@126.com>
2018-03-06 15:01:36 +0800

    first from local to github project

---
+++ app/src/main/java/com/dupeng/git/gitstudydemo/GitStudyMainActivity.java
@@ -0,0 +1,13 @@
+package com.dupeng.git.gitstudydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitStudyMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_study_main);
+    }
+}
